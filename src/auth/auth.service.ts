c83c95cdc7be956edf135747e07e049696c05b96
rollsars <rollanoffical05@gmail.com>
2025-09-22 12:32:29 +0500

    auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/database/db.service';
 import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,20 @@ export class AuthService {
         private jwt: JwtService
     ){console.log('AuthService создан')}
 
-    async register(dto: RegisterDto){
-        const exeisting = await this.prisma
+    async register(@Body() dto: RegisterDto){
+        const existing = await this.prisma.user.findUnique({where: {email: dto.email}})
+        if (existing) throw new Error('Пользователь с этим адресом электронной почты уже существует.');
+        const hashed = await bcrypt.hash(dto.password, 10);
+        if(dto.role === 'TEACHER'){
+            const user = await this.prisma.user.create({
+                data: {
+                    fullname: dto.fullname,
+                    email: dto.email,
+                    password: dto.password,
+                    role: 'TEACHER',
+
+                }
+            })
+        }
     }
 }
