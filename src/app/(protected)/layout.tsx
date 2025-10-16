1d049751a5014d965e86303c2c01048012ba9cfc
rollsars <rollanoffical05@gmail.com>
2025-10-16 18:29:13 +0500

    поправил глабал цсс

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function ProtectedLayout({children}: {children:ReactNode}){
     return(
-        <div className="bg-black">
+        <div className="bg-gray/500">
             {children}
         </div>
     )
